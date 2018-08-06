ce6918b827f9618096cbc81a43a00e704922674e
ajaykhonadeloitte <akhona@deloitte.com>
2018-08-06 16:25:09 +0530

    Initial Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    // this.wow.init({ live: false });
+
+  }
+}
